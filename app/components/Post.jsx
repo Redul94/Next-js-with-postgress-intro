e2802a9aa2c140ea8edbaf4c0f06467513f27d7c
Redul94 <redul15-3193@diu.edu.bd>
2024-06-20 22:37:24 +0600

    form add

---
+++ app/components/Post.jsx
@@ -0,0 +1,15 @@
+export default function Post({id,tittle,content,authorName}){
+    return (
+        <div style={{border: '1px solid white',padding:'15px',margin:'10px'}}>
+            <h3>
+                {authorName}
+            </h3>
+            <h4>
+                {tittle}
+            </h4>
+            <p>
+                {content}
+            </p>
+        </div>
+    )
+}
---
+++ app/page.jsx
@@ -0,0 +1,38 @@
+import prisma from "@/lib/prisma";
+import styles from "./page.module.css";
+import Post from "./components/Post";
+import Link from "next/link";
+
+async function getPosts(){
+  const posts = await prisma.post.findMany({
+    where : {published : true},
+    include : {
+      author : {
+        select : { name: true}
+      }
+    }
+  })
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
+
+  return <main className={styles.main}>
+    <Link href={'/addPost'}>Add Post</Link>
+    <h1>Feed</h1>
+    {
+      posts.map((post)=> {
+        return (
+          <Post 
+          key={post.id}
+          id={post.id}
+          tittle={post.tittle}
+          content={post.content}
+          authorName={post.author.name}
+          />
+        )
+      })
+    }
+  </main>;
+}
